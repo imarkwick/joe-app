fbeb13e25012db3985cd0d4f71dbcd168c35133f
imarkwick <izzymarkwick@gmail.com>
2015-03-04 13:58:49 +0000

    sinatra server and simple frontend mockup

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('#track-one').click(function() {
+		$(this).toggleClass('onetoggle');
+	});
+	$('#track-two').click(function() {
+		$(this).toggleClass('twotoggle');
+	});
+	$('#track-three').click(function() {
+		$(this).toggleClass('threetoggle');
+	});
+});
+
