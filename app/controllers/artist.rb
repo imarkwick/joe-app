082cd7ca1b6178b74afb419e1d6069c90f9e2cfc
imarkwick <izzymarkwick@gmail.com>
2015-03-14 23:41:34 +0000

    gig listings on the page

--- app/controllers/artist.rb
+++ app/controllers/artist.rb
@@ -1,10 +1,17 @@
 get '/artist' do
 	@tracks = Track.all
+	@gigs = Gig.all
 	erb :artist
 end
 
 get '/artist/:id' do
 	remove_track = Track.get(params[:id])
 	remove_track.destroy
+	redirect '/artist'
+end
+
+get '/artist/gig/:id' do
+	remove_gig = Gig.get(params[:id])
+	remove_gig.destroy
 	redirect '/artist'
 end
