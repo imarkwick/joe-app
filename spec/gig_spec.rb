3a3fca82541ff5e6eee71ae467bc95768a1f5f48
imarkwick <izzymarkwick@gmail.com>
2015-04-21 11:14:51 +0100

    clear background

--- spec/gig_spec.rb
+++ spec/gig_spec.rb
@@ -1,16 +1,15 @@
-require 'spec_helper' do
+require 'spec_helper'
 
-	feature Gig do
+feature Gig do
 
-		scenario 'should save and retrieve gigs from the database' do
-			expect(Gig.count).to eq(0)
-			Gig.create(venue: 'XOYO', date: '15.04.15')
-			expect(Gig.count).to eq(1)
-			gig = Gig.first
-			expect(gig.venue).to eq('XOYO')
-			expect(gig.date).to eq('15.04.15')
-			gig.destroy
-			expect(Gig.count).to eq(0)
-		end
+	scenario 'should save and retrieve gigs from the database' do
+		expect(Gig.count).to eq(0)
+		Gig.create(venue: 'XOYO', date: '15.04.15')
+		expect(Gig.count).to eq(1)
+		gig = Gig.first
+		expect(gig.venue).to eq('XOYO')
+		expect(gig.date).to eq('15.04.15')
+		gig.destroy
+		expect(Gig.count).to eq(0)
 	end
 end
